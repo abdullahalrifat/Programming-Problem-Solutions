26469959548e9750dbe06a367e8469dde56fe5a6
Abdullah Al Rifat <abdullahalrifat95@gmail.com>
2020-01-25 15:11:46 +0600

    hash code 2020

---
+++ HashCode_2020/PractiseRound/slice.cpp
@@ -0,0 +1,53 @@
+// CPP program to find Minimum number of times A
+// has to be repeated such that B is a substring of it
+#include <bits/stdc++.h>
+
+#define input freopen("a_example.in", "r", stdin);
+#define output freopen("a_example.out", "w", stdout);
+#define sync            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+bool sortbysec(const pair<int,int> &a,
+               const pair<int,int> &b)
+{
+    return (a.second > b.second);
+}
+int main()
+{
+    //sync;
+    //input;
+    //output;
+    //freopen("a_example.in", "r", stdin);
+    //freopen("a_example.out", "w", stdout);
+    int max, type;
+    cin>>max>>type;
+    vector<int> types;
+    vector<pair<int,int>> list;
+    int totalTypes = 0;
+    for(int i=0;i<type;i++)
+    {
+        int temp;
+        cin>>temp;
+        types.push_back(temp);
+        list.emplace_back(make_pair(i,temp));
+    }
+    sort(list.begin(), list.end(), sortbysec);
+    int cnt = 0;
+    vector<pair<int,int>> finalList;
+    for(int i=0;i<list.size();i++)
+    {
+        if(list[i].second > max) continue;
+        if(list[i].second + cnt > max) continue;
+        cnt += list[i].second;
+        finalList.emplace_back(list[i]);
+    }
+    sort(finalList.begin(), finalList.end());
+    cout<<finalList.size()<<endl;
+    for(int i=0;i<finalList.size();i++)
+    {
+        cout<<finalList[i].first<<" ";
+    }
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
